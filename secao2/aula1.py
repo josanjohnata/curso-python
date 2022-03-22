4f79db2d12421e4b187690aabc4001e9bdbea5b0
josanjohnata <josanjohnata@gmail.com>
2022-03-22 20:43:32 -0300

    Aula 1 - Comentarios

---
+++ secao2/aula1.py
@@ -0,0 +1,11 @@
+# O simnolo de cerquilha(#) é usado para comentar uma linha
+print("Hello World")  # Comentário do lado do código precisa ser feito depois
+# de 2 espaços
+"""
+Quando usamos 3 aspas, podemos comentar em
+mais de uma linha, mas geralmente usamos
+para documentar o novo projeto.
+"""
+'''
+A mesma coisa para as 3 aspas simples
+'''
