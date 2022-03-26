810b99e37f9b227f36a3a783a410b68f56e3f0b6
josanjohnata <josanjohnata@gmail.com>
2022-03-26 20:25:45 -0300

    Oque e uma STRING?

---
+++ secao2/aula3.py
@@ -0,0 +1,8 @@
+# String em Python é um tipo primitivo de dados, que é um conjunto de
+# caracteres que estão dentro de aspas. Exemplo: str - string
+print('Hello World')
+print("Hello World")
+# Python é uma linguagem de tipagem dinâmica, ou seja, quando escrevemos
+# o caracteres entre astas, o Python entende que é uma string. Quando
+# escrevemos números fora de aspas ele entende que é um int.
+print(12345)
