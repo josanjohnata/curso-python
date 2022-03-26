1092ba5bc4e2a4048b692ff5b725a525adf6e598
josanjohnata <josanjohnata@gmail.com>
2022-03-26 20:06:35 -0300

    Funçao print()

---
+++ secao2/aula2.py
@@ -0,0 +1,11 @@
+# Função print() para imprimir na tela argumentos, função, variável e valores
+# print("Hello World!")
+# O argumento nomeado sep="" separa os argumentos com o argumento que for
+# declarado entre aspas, o argumento end="" define o final da linha.
+print("Hello", "World!", sep="-", end="\n")
+"""
+Exercício: Usando o print(), para gerar um CPF, como mostrado no exemplo:
+123.456.789-10
+"""
+print("123", "456", "789", sep=".", end="-")
+print("10")
