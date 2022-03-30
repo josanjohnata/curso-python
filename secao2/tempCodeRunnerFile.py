7a95e391204322fb12e254014247b92e7f64831a
josanjohnata <josanjohnata@gmail.com>
2022-03-30 19:19:03 -0300

    Aula 4 - Tipos de dados 'Primitivos'

--- secao2/tempCodeRunnerFile.py
+++ secao2/tempCodeRunnerFile.py
@@ -1,2 +1,13 @@
-print("123", "456", "789", sep=".", end="-")
-print("10")
+# Exercícios:
+# Exiba na tela o tipo de dado de cada dado do cadastro abaixo.
+# String: nome
+print("Josan", type("Josan"))
+
+# Inteiro: idade
+print(30, type(30))
+
+# Float: altura
+print(1.76, type(1.76))
+
+# Booleano: se é maior de idade
+print(30 > 18, type(30 > 18))
