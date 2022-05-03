fe604921ea8538083c6f6e81017f774c75ddd164
josanjohnata <josanjohnata@gmail.com>
2022-05-03 20:35:55 -0300

    Operadores Aritméticos

---
+++ secao2/aula5.py
@@ -0,0 +1,14 @@
+# Operadores Aritméticos
+
+"""
++, -, *, /, //, **, %, ()
+"""
+print("Multiplicação: *", 10 * 10)
+print("Adição: +", 10 + 10)
+print("Subtração: -", 10 - 5)
+print("Divisão: /", 10 / 2)  # Retorna a divisão real do número
+print("Divisão Inteira: //", 10 // 10)  # Retorna a divisão inteira do número
+print("Exponenciação: **", 2 ** 10)  # Retorna o número elevado ao expoente
+print("Resto da Divisão: %", 10 % 5)  # Retorna o resto da divisão
+print("Soma de Parenteses: ()", 5 + (10 * 10))  # Retorna a soma dos números 
+# entre parenteses
