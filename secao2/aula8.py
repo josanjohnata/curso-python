c47a934b1235788eb903d4f400c54d95d2b43ce3
josanjohnata <josanjohnata@gmail.com>
2022-05-06 20:32:48 -0300

    Entrada de dados - input

---
+++ secao2/aula8.py
@@ -0,0 +1,12 @@
+# Entrada de dados
+
+# O retorno de um input SEMPRE é uma string
+input("Qual o seu nome? ")
+
+nome = input("Qual o seu nome? ")
+print("Olá", nome)
+
+# Para converter para inteiro temos que fazer um cast para int
+idade = int(input("Qual a sua idade? "))
+numero = input("Qual o seu número? ")
+numero = int(numero)
